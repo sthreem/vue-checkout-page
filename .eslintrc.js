cb22215759475a21b7ff0de2c9d8b391f542663a
Stefan Milivojevic <milivojevic.ste@gmail.com>
2021-06-29 21:27:37 -0400

    ESLint and Prettier applied on basic project

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        indent: ['error', 4]
+        indent: ['error', 4],
+        'prettier/prettier': ['off', { endOfLine: 'crlf' }]
     },
     overrides: [
         {
