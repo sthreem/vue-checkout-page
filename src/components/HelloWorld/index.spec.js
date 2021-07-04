5ed7e57e31f7cf7499db788d4464b44c4eb5c05a
Stefan Milivojevic <milivojevic.ste@gmail.com>
2021-07-04 00:28:30 -0400

    Cleanup files

--- src/components/HelloWorld/index.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld/Index.vue';
-
-describe('HelloWorld component', () => {
-    it('should render properly', () => {
-        const wrapper = shallowMount(HelloWorld);
-        expect(wrapper.exists()).toBe(true);
-    });
-});
